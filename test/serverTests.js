6253efd5fa1cee2fd26b7916c9acd0bd7186e883
thejsf <joe.forbes@gmail.com>
2015-05-04 19:56:09 -0500

    Test verifying application name and version in first line of console output.

--- test/serverTests.js
+++ test/serverTests.js
@@ -1,12 +1,38 @@
 var should = require('chai').should(),
-    stdout = require('test-console').stdout,
-    server;
+    exec = require('child_process').exec,
+    path = require('path'),
+    packageJson = require('../package.json');
 
 describe('server tests', function () {
 
     it('should not blow up when the server is invoked by \'require\'', function () {
-        server = require('../server');
+        var server = require('../server');
+    });
+
+    it('should log an info message with the application version as the first message logged', function(done) {
+        var cmd,
+            opt,
+            inspectOutput,
+            parentDir;
+
+        cmd = 'npm start';
+
+        inspectOutput = (
+            function (stderr, stdout) {
+                var output, pattern, regExp;
+                should.not.exist(stderr);
+                should.exist(stdout);
+                output = stdout.split('\n');
+                output.length.should.be.at.least(5);
+
+                pattern = packageJson.name + ' ' + packageJson.version + '$';
+                regExp = new RegExp(pattern);
+                output[4].should.match(regExp);
+                done();
+            }
+        );
+
+        exec(cmd, null, inspectOutput);
     });
 
 });
-
