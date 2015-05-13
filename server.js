e6cd00ed6f00c9212fc52d88a01cae2c9c3d0c0d
thejsf <joe.forbes@gmail.com>
2015-05-12 22:55:58 -0500

    Finished startup log verification tests.

--- server.js
+++ server.js
@@ -8,3 +8,5 @@ config = configger.load();
 logger.addTargets(config.loggingTargets);
 
 logger.info(packageJson.name + ' ' + packageJson.version);
+logger.debug({'package.json': packageJson});
+logger.debug({'config': config});
--- test/serverTests.js
+++ test/serverTests.js
@@ -1,19 +1,161 @@
-var should = require('chai').should(),
+var chai = require('chai'),
+    assert = chai.assert,
+    should = chai.should(),
     exec = require('child_process').exec,
-    path = require('path'),
-    packageJson = require('../package.json');
+    packageJson = require('../package.json'),
+    fs = require('fs'),
+    LineByLine = require('line-by-line');
 
 describe('server tests', function () {
 
     it('should not blow up when the server is invoked by \'require\'', function () {
-        var server = require('../server');
+        require('../server');
     });
 
-    it('should log an info message with the application version as the first message logged', function(done) {
+    it('should log a debug message with the application package.json', function (done) {
+
+        if (fs.existsSync('./test-log.json')) {
+            fs.unlinkSync('./test-log.json');
+        }
+
         var cmd,
-            opt,
             inspectOutput,
-            parentDir;
+            programStartTime;
+
+        cmd = 'configFile=test/resources/debugConfig.json npm start';
+
+        inspectOutput = (
+            function (stderr) {
+                var logReader, found;
+                should.not.exist(stderr);
+                logReader = new LineByLine('./test-log.json');
+                logReader.on('error', function (err) {
+                    throw new Error('Error raised by logReader: ' + err);
+                });
+                logReader.on('line', function (line) {
+                    var logEntry = JSON.parse(line);
+                    logEntry.date = new Date(logEntry.timestamp);
+                    if (logEntry.level = 'debug' && logEntry.date > programStartTime) {
+                        if (logEntry.hasOwnProperty('package.json')) {
+                            logEntry['package.json'].should.eql(packageJson);
+                            logReader.close();
+                            found = true;
+                            done();
+                        }
+                    }
+                });
+                logReader.on('end', function () {
+                    if (!found) {
+                        assert.fail('log end reached without finding expected message', 'log message searched for found');
+                    }
+                });
+            }
+        );
+
+        programStartTime = new Date();
+        exec(cmd, null, inspectOutput);
+    });
+
+    it('should log a debug message with the application config', function (done) {
+
+        if (fs.existsSync('./test-log.json')) {
+            fs.unlinkSync('./test-log.json');
+        }
+
+        var cmd,
+            inspectOutput,
+            programStartTime;
+
+        cmd = 'configFile=test/resources/debugConfig.json npm start';
+
+        inspectOutput = (
+            function (stderr) {
+                var logReader, found,
+                    config = require('./resources/debugConfig.json');
+                config.configFile = 'test/resources/debugConfig.json';
+                config.type = 'literal';
+                config.$0 = 'node ./server.js';
+                config._ = [];
+                should.not.exist(stderr);
+                logReader = new LineByLine('./test-log.json');
+                logReader.on('error', function (err) {
+                    throw new Error('Error raised by logReader: ' + err);
+                });
+                logReader.on('line', function (line) {
+                    var logEntry = JSON.parse(line);
+                    logEntry.date = new Date(logEntry.timestamp);
+                    if (logEntry.level = 'debug' && logEntry.date > programStartTime) {
+                        if (logEntry.hasOwnProperty('config')) {
+                            logEntry.config.should.eql(config);
+                            logReader.close();
+                            found = true;
+                            done();
+                        }
+                    }
+                });
+                logReader.on('end', function () {
+                    if (!found) {
+                        assert.fail('log end reached without finding expected message', 'log message searched for found');
+                    }
+                });
+            }
+        );
+
+        programStartTime = new Date();
+        exec(cmd, null, inspectOutput);
+    });
+
+    it('should log an info message with the application name and version', function (done) {
+
+        if (fs.existsSync('./test-log.json')) {
+            fs.unlinkSync('./test-log.json');
+        }
+
+        var cmd,
+            inspectOutput,
+            programStartTime;
+
+        cmd = 'configFile=test/resources/debugConfig.json npm start';
+
+        inspectOutput = (
+            function (stderr) {
+                var logReader, found,
+                    config = require('./resources/debugConfig.json');
+                config.configFile = 'test/resources/debugConfig.json';
+                config.type = 'literal';
+                config.$0 = 'node ./server.js';
+                config._ = [];
+                should.not.exist(stderr);
+                logReader = new LineByLine('./test-log.json');
+                logReader.on('error', function (err) {
+                    throw new Error('Error raised by logReader: ' + err);
+                });
+                logReader.on('line', function (line) {
+                    var logEntry = JSON.parse(line);
+                    logEntry.date = new Date(logEntry.timestamp);
+                    if (logEntry.level = 'info' && logEntry.date > programStartTime) {
+                        if (logEntry.message === packageJson.name + ' ' + packageJson.version) {
+                            logReader.close();
+                            found = true;
+                            done();
+                        }
+                    }
+                });
+                logReader.on('end', function () {
+                    if (!found) {
+                        assert.fail('log end reached without finding expected message', 'log message searched for found');
+                    }
+                });
+            }
+        );
+
+        programStartTime = new Date();
+        exec(cmd, null, inspectOutput);
+    });
+
+    it('should write the application version to stdout when started without arguments', function (done) {
+        var cmd,
+            inspectOutput;
 
         cmd = 'npm start';
 
@@ -34,5 +176,4 @@ describe('server tests', function () {
 
         exec(cmd, null, inspectOutput);
     });
-
 });
